8c0297e0d2063d21c5230fc24a172fd42720d8d8
tnalsvjscl <tnalsvjscl@naver.com>
2021-04-20 09:47:29 +0900

    build / be / 환경설정

---
+++ backend/wouldUmarryme/src/test/java/com/ssafy/wouldUmarryme/wouldUmarryme/WouldUmarrymeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.wouldUmarryme.wouldUmarryme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WouldUmarrymeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
