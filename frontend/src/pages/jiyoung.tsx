f3e230e7e5d1c7c929c636c84aabfb281e103008
Jiyoung <jayyyoung321@gmail.com>
2021-04-25 01:01:07 +0900

    feat / fe / stepProgressBar componenet 제작 / S04P31B106-83

--- frontend/src/pages/jiyoung.tsx
+++ frontend/src/pages/jiyoung.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import StepProgressBar from '../components/stepProgressBar/stepProgressBar';
 
 const Jiyoung = () => {
+  
+  
   return (
     <div>
       지영페이지
       <i className="fas fa-air-freshener"></i>
+      <StepProgressBar onClickHandler={() => {
+        console.log('hi');
+      }}/>
     </div>
   );
 }
