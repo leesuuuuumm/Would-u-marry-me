66250d329f14ab40e2eca3343c61273ac80e5304
Jiyoung <jayyyoung321@gmail.com>
2021-04-24 22:47:20 +0900

    feat / fe / fontawesome 추가 / S04P31B106-81

--- frontend/src/pages/jiyoung.tsx
+++ frontend/src/pages/jiyoung.tsx
@@ -2,7 +2,10 @@ import React from 'react';
 
 const Jiyoung = () => {
   return (
-    <div>지영페이지</div>
+    <div>
+      지영페이지
+      <i className="fas fa-air-freshener"></i>
+    </div>
   );
 }
 
