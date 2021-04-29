2ce75417877f7cd1c04a04a4f50fce725f7e3ea7
JaeHyeok <roywogur@naver.com>
2021-04-30 03:57:00 +0900

    feat/fe/invitaion_template/S04P31B106-124

--- frontend/src/pages/jaehyuk.jsx
+++ frontend/src/pages/jaehyuk.jsx
@@ -2,14 +2,16 @@ import React from 'react';
 import WeddingTemplate from '../components/weddingTemplate/weddingTemplate1'
 import WeddingTemplate2 from '../components/weddingTemplate/weddingTemplate2'
 import WeddingTemplate3 from '../components/weddingTemplate/weddingTemplate3'
+import InvitationTemplate1 from '../components/invitationTemplate/invitationTemplate1'
 import styles from './jaehyuk.module.css'
 const Jaehyuk = () => {
   return (
     <>
     <div className={styles.backgournd_color}>
-      <WeddingTemplate></WeddingTemplate>
+      {/* <WeddingTemplate></WeddingTemplate>
       <WeddingTemplate2></WeddingTemplate2>
-      <WeddingTemplate3></WeddingTemplate3>
+      <WeddingTemplate3></WeddingTemplate3> */}
+      <InvitationTemplate1></InvitationTemplate1>
     </div>
     </>
   );
