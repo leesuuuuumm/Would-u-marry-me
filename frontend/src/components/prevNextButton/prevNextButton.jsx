c285ab4cd1876337c28fd922dde8a4180e941bd8
Jiyoung <jayyyoung321@gmail.com>
2021-05-01 01:33:18 +0900

    fix / fe / prevNextButton 컴포넌트 css 변수명 변경 / S04P31B106-139

--- frontend/src/components/prevNextButton/prevNextButton.jsx
+++ frontend/src/components/prevNextButton/prevNextButton.jsx
@@ -3,7 +3,7 @@ import styles from './prevNextButton.module.css';
 
 export const PrevNextButton = ({ color }) => {
   return (
-    <div className={`${styles['prev-next-button']} ${color === "white" && styles.colorWhite}`}>
+    <div className={`${styles['prev-next-button']} ${color === "white" && styles['color-white']}`}>
       <i className="fas fa-arrow-circle-left"></i>
       <i className="fas fa-arrow-circle-right"></i>
     </div>
@@ -12,7 +12,7 @@ export const PrevNextButton = ({ color }) => {
 
 export const NextButton = ({ color }) => {
   return (
-    <div className={`${styles['next-button']} ${color === "white" && styles.colorWhite}`}>
+    <div className={`${styles['next-button']} ${color === "white" && styles['color-white']}`}>
       <i className="fas fa-arrow-circle-right"></i>
     </div>
   );
