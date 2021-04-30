8a876fcb5e016891c53ff033f9a74f9e980c2c7d
Jiyoung <jayyyoung321@gmail.com>
2021-05-01 01:32:42 +0900

    fix / fe / moveStoryBoardButton 컴포넌트 css 변수명 변경 / S04P31B106-138

--- frontend/src/components/moveStoryBoardButton/moveStoryBoardButton.jsx
+++ frontend/src/components/moveStoryBoardButton/moveStoryBoardButton.jsx
@@ -4,7 +4,7 @@ import styles from './moveStoryBoardButton.module.css';
 const MoveStoryBoardButton = ({ color }) => {
 
   return (
-    <div className={`${styles['move-story-board-button']} ${color === "white" && styles.colorWhite}`}>
+    <div className={`${styles['move-story-board-button']} ${color === "white" && styles['color-white']}`}>
       <i className="fas fa-th"></i>
     </div>
   );
