f81d29374c1c1dcf06dad27313d6353ee79ecb9b
Jiyoung <jayyyoung321@gmail.com>
2021-05-09 23:37:23 +0900

    feat / fe / stepProgressBar 수정 / S04P31B106-83

--- frontend/src/components/stepProgressBar/stepProgressBar.jsx
+++ frontend/src/components/stepProgressBar/stepProgressBar.jsx
@@ -2,62 +2,50 @@ import React, { useState } from 'react';
 import styles from './stepProgressBar.module.css';
 
 
-const StepProgressBar = ({onClickHandler}) => {
-  const [stepNumber, setStepNumber] = useState(1);
+const StepProgressBar = ({ currentStep }) => {
 
-  const moveStep = (newStepNumber) => {
-    setStepNumber(newStepNumber);
+  const moveStep = () => {
+    
     // props 로 내려받은 함수 실행
-    onClickHandler();
+  
   };
 
   return (
     <div className={styles['step-progress-bar']}>
       <button 
-        className={`${styles['step-circle']} ${stepNumber === 1 && styles.active}`}
-        onClick={() => {moveStep(1)}}
-      >
-        1
-      </button>
+        className={`${styles['step-circle']} ${currentStep === 0 && styles['current-step']}`}
+      >1</button>
       <div className={styles['step-line']}></div>
       <button 
-        className={`${styles['step-circle']} ${stepNumber === 2 && styles.active}`}
-        onClick={() => {moveStep(2)}}
+        className={`${styles['step-circle']} ${currentStep === 1 && styles['current-step']}`}
       >2</button>
       <div className={styles['step-line']}></div>
       <button 
-        className={`${styles['step-circle']} ${stepNumber === 3 && styles.active}`}
-        onClick={() => {moveStep(3)}}
+        className={`${styles['step-circle']} ${currentStep === 2 && styles['current-step']}`}
       >3</button>
       <div className={styles['step-line']}></div>
       <button 
-        className={`${styles['step-circle']} ${stepNumber === 4 && styles.active}`}
-        onClick={() => {moveStep(4)}}
+        className={`${styles['step-circle']} ${currentStep === 3 && styles['current-step']}`}
       >4</button>
       <div className={styles['step-line']}></div>
       <button 
-        className={`${styles['step-circle']} ${stepNumber === 5 && styles.active}`}
-        onClick={() => {moveStep(5)}}
+        className={`${styles['step-circle']} ${currentStep === 4 && styles['current-step']}`}
       >5</button>
       <div className={styles['step-line']}></div>
       <button 
-        className={`${styles['step-circle']} ${stepNumber === 6 && styles.active}`}
-        onClick={() => {moveStep(6)}}
+        className={`${styles['step-circle']} ${currentStep === 5 && styles['current-step']}`}
       >6</button>
       <div className={styles['step-line']}></div>
       <button 
-        className={`${styles['step-circle']} ${stepNumber === 7 && styles.active}`}
-        onClick={() => {moveStep(7)}}
+        className={`${styles['step-circle']} ${currentStep === 6 && styles['current-step']}`}
       >7</button>
       <div className={styles['step-line']}></div>
       <button 
-        className={`${styles['step-circle']} ${stepNumber === 8 && styles.active}`}
-        onClick={() => {moveStep(8)}}
+        className={`${styles['step-circle']} ${currentStep === 7 && styles['current-step']}`}
       >8</button>
       <div className={styles['step-line']}></div>
       <button 
-        className={`${styles['step-circle']} ${stepNumber === 9 && styles.active}`}
-        onClick={() => {moveStep(9)}}
+        className={`${styles['step-circle']} ${currentStep === 8  && styles['current-step']}`}
       >9</button>
     </div>
   );
