4dbfe346d8c0f37a2e4c6f183f2d5475b09839fe
Jiyoung <jayyyoung321@gmail.com>
2021-05-09 23:35:55 +0900

    feat / fe / prevNextButton 수정 / S04P31B106-139

--- frontend/src/components/prevNextButton/prevNextButton.jsx
+++ frontend/src/components/prevNextButton/prevNextButton.jsx
@@ -1,27 +1,24 @@
 import React from 'react';
 import styles from './prevNextButton.module.css';
 
-export const PrevNextButton = ({ color }) => {
-  return (
-    <div className={`${styles['prev-next-button']} ${color === "white" && styles['color-white']}`}>
-      <i className="fas fa-arrow-circle-left"></i>
-      <i className="fas fa-arrow-circle-right"></i>
-    </div>
-  );
-};
 
-export const NextButton = ({ color }) => {
+export const NextButton = ({ color, moveNextStep }) => {
+
+
   return (
-    <div className={`${styles['next-button']} ${color === "white" && styles['color-white']}`}>
+    <button 
+      className={`${styles['next-button']} ${color === "white" && styles['color-white']}`}
+      onClick={() => {moveNextStep()}}
+    >
       <i className="fas fa-arrow-circle-right"></i>
-    </div>
+    </button>
   );
 };
 
 export const PrevButton = ({ color }) => {
   return (
-    <div className={`${styles['prev-button']} ${color === "white" && styles.colorWhite}`}>
+    <button className={`${styles['prev-button']} ${color === "white" && styles.colorWhite}`}>
       <i className="fas fa-arrow-circle-left"></i>
-    </div>
+    </button>
   )
 }
