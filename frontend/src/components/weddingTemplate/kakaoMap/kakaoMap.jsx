88ec60dd2c0fd436ae884a67869a80d26eaa1196
psjoon33 <tjdwnsdl0303@gmail.com>
2021-05-12 22:25:29 +0900

    feat / fe / 스토리, 웨딩 템플릿 호버 이벤트 / S04P31B106-199

--- frontend/src/components/weddingTemplate/kakaoMap/kakaoMap.jsx
+++ frontend/src/components/weddingTemplate/kakaoMap/kakaoMap.jsx
@@ -1,19 +1,20 @@
-import React, { useState } from 'react';
-import styles from './kakaoMap.module.css'
-import { useEffect } from 'react';
+import React, { useState } from "react";
+import styles from "./kakaoMap.module.css";
+import { useEffect } from "react";
 
 const { kakao } = window;
 const KakaoMap = (props) => {
-  const {searchPlace} = props
-  var mapInfo = props.mapInfo
-  var mapExist = props.mapExist
+  const { searchPlace } = props;
+  var mapInfo = props.mapInfo;
+  var mapExist = props.mapExist;
+  console.log(mapExist);
   useEffect(() => {
     if (mapExist === undefined) {
-      var mapContainer = document.getElementById('myMap'), // 지도를 표시할 div
-      mapOption = { 
+      var mapContainer = document.getElementById("myMap"), // 지도를 표시할 div
+        mapOption = {
           center: new kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
-          level: 3 // 지도의 확대 레벨
-      };
+          level: 3, // 지도의 확대 레벨
+        };
       if (searchPlace) {
         const ps = new kakao.maps.services.Places();
         ps.keywordSearch(searchPlace, placesSearchCB);
@@ -32,58 +33,61 @@ const KakaoMap = (props) => {
       }
       var iwRemoveable = true; // removeable 속성을 ture 로 설정하면 인포윈도우를 닫을 수 있는 x버튼이 표시됩니다
       var infowindow = new kakao.maps.InfoWindow({
-        removable : iwRemoveable
+        removable: iwRemoveable,
       });
       function displayMarker(place) {
         var marker = new kakao.maps.Marker({
           map: map,
           position: new kakao.maps.LatLng(place.y, place.x),
-          clickable: true 
+          clickable: true,
         });
-        kakao.maps.event.addListener(marker, 'click', function() {
+        kakao.maps.event.addListener(marker, "click", function () {
           // 마커 위에 인포윈도우를 표시합니다
-          infowindow.setContent('<div style="padding:5px;font-size:12px;">' + place.place_name + '</div>')
-          infowindow.open (map, marker);
+          infowindow.setContent(
+            '<div style="padding:5px;font-size:12px;">' +
+              place.place_name +
+              "</div>"
+          );
+          infowindow.open(map, marker);
           var info = {
-            name : place.place_name,
-            y : place.y,
-            x : place.x,
-          }
-          getSearchInfo(info)
+            name: place.place_name,
+            y: place.y,
+            x: place.x,
+          };
+          getSearchInfo(info);
         });
       }
-      var map = new kakao.maps.Map(mapContainer,  mapOption); // 지도를 생성합니다
-    }
-    else {
-      var mapContainer = document.getElementById('myMap'), // 지도를 표시할 div
-      mapOption = { 
+      var map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
+    } else {
+      var mapContainer = document.getElementById("myMap"), // 지도를 표시할 div
+        mapOption = {
           center: new kakao.maps.LatLng(mapInfo.y, mapInfo.x), // 지도의 중심좌표
-          level: 3 // 지도의 확대 레벨
-      };
-      var map = new kakao.maps.Map(mapContainer,  mapOption); // 지도를 생성합니다
-      var markerPosition  = new kakao.maps.LatLng(mapInfo.y, mapInfo.x); 
+          level: 3, // 지도의 확대 레벨
+        };
+      var map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
+      var markerPosition = new kakao.maps.LatLng(mapInfo.y, mapInfo.x);
       var marker = new kakao.maps.Marker({
-        position: markerPosition
+        position: markerPosition,
       });
       marker.setMap(map);
     }
-
   }, [searchPlace]);
   const getSearchInfo = (info) => {
-    props.getSearchInfo(info)
-  }
-return (
-  <>
-    { mapExist=== undefined
-    ?
-    <div id='myMap' className={styles['map']}></div>
-    :
-    <div id='myMap'  className={styles['map-searched']}></div>
-    // <div   className={styles['map-searched']}>dddd</div>
-    // <div id='myMap' style="width:100%;height:100%;" className={styles['map-searched']}></div>
-    }
-  </>
-);
-}
+    props.getSearchInfo(info);
+  };
+  return (
+    <>
+      {
+        mapExist === undefined ? (
+          <div id="myMap" className={styles["map"]}></div>
+        ) : (
+          <div id="myMap" className={styles["map-searched"]}></div>
+        )
+        // <div   className={styles['map-searched']}>dddd</div>
+        // <div id='myMap' style="width:100%;height:100%;" className={styles['map-searched']}></div>
+      }
+    </>
+  );
+};
 
 export default KakaoMap;
--- frontend/src/components/weddingTemplate/weddingTemplate3/weddingTemplate3.jsx
+++ frontend/src/components/weddingTemplate/weddingTemplate3/weddingTemplate3.jsx
@@ -1,167 +1,271 @@
-import React, {useState} from 'react';
-import styles from './weddingTemplate3.module.css'
-import SearchPlace from '../kakaoMap/searchPlace'
+import React, { useState } from "react";
+import styles from "./weddingTemplate3.module.css";
+import SearchPlace from "../kakaoMap/searchPlace";
 import KakaoMap from "../kakaoMap/kakaoMap";
 
 const WeddingTemplate3 = () => {
   const [img, setImg] = useState();
   const [imgFile, setImgFile] = useState();
+  const [imgInput1, setImgInput1] = useState(false);
+  const [mouseCheck1, setMouseCheck1] = useState(false);
+  const [mouseCheck2, setMouseCheck2] = useState(false);
+  const [mouseCheck3, setMouseCheck3] = useState(false);
+  const [imageHoverCheck, setImageHoverCheck] = useState(false);
   const [mapOpen, setMapOpen] = useState(false);
   const [mapInfo, setMapInfo] = useState({});
   const [mapExist, setMapExist] = useState(false);
-  var date = ""
-  var time = ""
-  var place = ""
+  var date = "";
+  var time = "";
+  var place = "";
 
+  const mouseOn1 = () => {
+    setMouseCheck1(!mouseCheck1);
+  };
+  const mouseOn2 = () => {
+    setMouseCheck2(!mouseCheck2);
+  };
+  const mouseOn3 = () => {
+    setMouseCheck3(!mouseCheck3);
+  };
+  const imageMouseOn = () => {
+    setImageHoverCheck(true);
+  };
+  const imageMouseOut = () => {
+    setImageHoverCheck(false);
+  };
   const getMapInfo = (mapInfo) => {
     setMapOpen(false);
-    setMapInfo(mapInfo)
-    setMapExist(!mapExist)
-  }
+    setMapInfo(mapInfo);
+    setMapExist(!mapExist);
+  };
   const openMapModal = () => {
     setMapOpen(true);
-  }
+  };
   const closeMapModal = () => {
     setMapOpen(false);
-  }
+  };
   const onImageChange = function (e) {
-    console.log('hi')
-
     setImgFile(e.target.files[0]);
     setImg(URL.createObjectURL(e.target.files[0]));
+    if (!imgInput1) {
+      setImgInput1(!imgInput1);
+    }
   };
   const onDateChange = function (e) {
-    console.log(e.target.value)
-    date = e.target.value
-    console.log(date)
+    console.log(e.target.value);
+    date = e.target.value;
+    console.log(date);
   };
   const onTimeChange = function (e) {
-    console.log(e.target.value)
-    time = e.target.value
-    console.log(time)
+    console.log(e.target.value);
+    time = e.target.value;
+    console.log(time);
   };
   const onPlaceChange = function (e) {
-    console.log(e.target.value)
-    place = e.target.value
-    console.log(place)
+    console.log(e.target.value);
+    place = e.target.value;
+    console.log(place);
   };
-  
+
   return (
-    <div className={styles['template-box']}>
-      <div className={styles['input-area1']}>
-        <div className={styles['text-box1']}>
-          <button className={styles['text-button1']}>
-            <div className={styles['text-icon1']}>
+    <div className={styles["template-box"]}>
+      <div className={styles["input-area1"]}>
+        <div className={styles["text-box1"]}>
+          <button className={styles["text-button1"]}>
+            <div className={styles["text-icon1"]}>
               <i className="fas fa-pencil-alt"></i>
             </div>
           </button>
         </div>
-        <div className={styles['image-box']}>
-          <img src={img}/>
-            <label className={styles['image-button']}>
-              <div className={styles['image-icon']}>
+        {/* 사진 입력 */}
+        {imgInput1 === false ? (
+          <div className={styles["image-box"]}>
+            <img src={img} />
+            <label className={styles["image-button"]}>
+              <div className={styles["image-icon"]}>
                 <i className="fas fa-camera"></i>
-                  <input
-                    type="file"
-                    className={styles['image-input']}
-                    onChange={onImageChange}
-                  />
+                <input
+                  type="file"
+                  className={styles["image-input"]}
+                  onChange={onImageChange}
+                />
               </div>
             </label>
+          </div>
+        ) : (
+          // 입력 넣었을 때 - hover X
+          <div
+            className={styles["input-image-box"]}
+            onMouseEnter={imageMouseOn}
+            onMouseLeave={imageMouseOut}
+          >
+            {imageHoverCheck === false ? (
+              <label className={styles["input-image-done"]}>
+                <img src={img} className={styles["inputed-image"]} />
+                <input
+                  type="file"
+                  className={styles["image-input"]}
+                  onChange={onImageChange}
+                />
+              </label>
+            ) : (
+              <label className={styles["input-image-done"]}>
+                <img src={img} className={styles["inputed-image-on"]} />
+                <input
+                  type="file"
+                  className={styles["image-input"]}
+                  onChange={onImageChange}
+                />
+                <div
+                  className={styles["image-icon-done"]}
+                  onMouseEnter={imageMouseOn}
+                >
+                  <i className="fas fa-camera"></i>
+                </div>
+              </label>
+            )}
+          </div>
+        )}
+        {/* 날짜 입력 */}
+        <div
+          className={styles["date-button"]}
+          onMouseOver={mouseOn1}
+          onMouseOut={mouseOn1}
+        >
+          {mouseCheck1 === false ? (
+            <div className={styles["date-input-box"]}>
+              <input
+                className={styles["date-input"]}
+                type="date"
+                onChange={onDateChange}
+              />
+            </div>
+          ) : (
+            <div className={styles["date-input-box-on"]}>
+              <input
+                className={styles["date-input-on"]}
+                type="date"
+                onChange={onDateChange}
+              />
+            </div>
+          )}
         </div>
-        <div className={styles['date-button']}>
-            <input
-              className={styles['date-input']}
-              type="date"
-              onChange={onDateChange}
-            />
+        {/* 시각 입력 */}
+        <div
+          className={styles["date-button"]}
+          onMouseOver={mouseOn2}
+          onMouseOut={mouseOn2}
+        >
+          {mouseCheck2 === false ? (
+            <div className={styles["date-input-box"]}>
+              <input
+                className={styles["date-input2"]}
+                type="time"
+                onChange={onTimeChange}
+              />
+            </div>
+          ) : (
+            <div className={styles["date-input-box-on"]}>
+              <input
+                className={styles["date-input2-on"]}
+                type="time"
+                onChange={onTimeChange}
+              />
+            </div>
+          )}
         </div>
-        <div className={styles['date-button']}>
-            <input
-              className={styles['date-input']}
-              type="time"
-              onChange={onTimeChange}
-            />
-        </div>
-        <div className={styles['date-button']}>
-            <input
-              className={styles['date-input']}
-              type="text"
-              placeholder="Place"
-              onChange={onPlaceChange}
-            />
+        {/* 장소 입력 */}
+        <div
+          className={styles["date-button"]}
+          onMouseOver={mouseOn3}
+          onMouseOut={mouseOn3}
+        >
+          {mouseCheck3 === false ? (
+            <div className={styles["date-input-box"]}>
+              <input
+                className={styles["date-input3"]}
+                type="text"
+                placeholder="Place"
+                onChange={onPlaceChange}
+              />
+            </div>
+          ) : (
+            <div className={styles["date-input-box-on"]}>
+              <input
+                className={styles["date-input3-on"]}
+                type="text"
+                placeholder="Place"
+                onChange={onPlaceChange}
+              />
+            </div>
+          )}
         </div>
       </div>
-      <div className={styles['input-area2']}>
-        <div className={styles['text-box2']}>
-          <button className={styles['text-button2']}>
-            <div className={styles['text-icon2']}>
+      <div className={styles["input-area2"]}>
+        <div className={styles["text-box2"]}>
+          <button className={styles["text-button2"]}>
+            <div className={styles["text-icon2"]}>
               <i className="fas fa-pencil-alt"></i>
             </div>
           </button>
         </div>
-        <div className={styles['info-man']}>
-          <div className={styles['info-man-title']}>신랑</div>
-          <div className={styles['info-man-text-area']}>
-            <input type="text"
-              className={styles['info-man-text']}
+        <div className={styles["info-man"]}>
+          <div className={styles["info-man-title"]}>신랑</div>
+          <div className={styles["info-man-text-area"]}>
+            <input
+              type="text"
+              className={styles["info-man-text"]}
               placeholder="PhoneNumber"
             />
-            <input type="text"
-              className={styles['info-man-text']}
+            <input
+              type="text"
+              className={styles["info-man-text"]}
               placeholder="AccountNumber"
             />
           </div>
         </div>
-        <div className={styles['info-woman']}>
-          <div className={styles['info-woman-title']}>신부</div>
-            <div className={styles['info-woman-text-area']}>
-              <input type="text"
-                className={styles['info-woman-text']}
-                placeholder="PhoneNumber"
-              />
-              <input type="text"
-                className={styles['info-woman-text']}
-                placeholder="AccountNumber"
-              />
-            </div>
+        <div className={styles["info-woman"]}>
+          <div className={styles["info-woman-title"]}>신부</div>
+          <div className={styles["info-woman-text-area"]}>
+            <input
+              type="text"
+              className={styles["info-woman-text"]}
+              placeholder="PhoneNumber"
+            />
+            <input
+              type="text"
+              className={styles["info-woman-text"]}
+              placeholder="AccountNumber"
+            />
+          </div>
         </div>
-        { mapExist === false
-          ?
-          <div className={styles['map-box']}>
-            <button
-              className={styles['text-button2']}
-              onClick={openMapModal}
-            >
-              <div className={styles['map-icon']}>
+        {mapExist === false ? (
+          <div className={styles["map-box"]}>
+            <button className={styles["text-button2"]} onClick={openMapModal}>
+              <div className={styles["map-icon"]}>
                 <i className="fas fa-map-marked-alt"></i>
               </div>
             </button>
           </div>
-          :
+        ) : (
           <>
-          <div className={styles['map-serached']}>
-            <KakaoMap 
-              mapInfo={mapInfo}
-              mapExist={mapExist}  
-            >
-            </KakaoMap>
-          </div>
+            <div className={styles["map-serached"]}>
+              <KakaoMap mapInfo={mapInfo} mapExist={mapExist}></KakaoMap>
+            </div>
           </>
-        }
+        )}
       </div>
-      { mapOpen === false
-        ?
-        ''
-        :
-        <div className={styles['map-component']}>
+      {mapOpen === false ? (
+        ""
+      ) : (
+        <div className={styles["map-component"]}>
           <SearchPlace
-            open = { mapOpen }
-            close = { closeMapModal }
-            getMapInfo = {getMapInfo}
+            open={mapOpen}
+            close={closeMapModal}
+            getMapInfo={getMapInfo}
           ></SearchPlace>
         </div>
-        }
+      )}
     </div>
   );
 };
