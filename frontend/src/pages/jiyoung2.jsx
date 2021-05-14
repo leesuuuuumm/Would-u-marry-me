931761fdfa02173491090879928171c11c6591ab
Jiyoung <jayyyoung321@gmail.com>
2021-05-14 15:20:41 +0900

    feat / fe / weddingTemplate 수정test / S04P31B106-216

--- frontend/src/pages/jiyoung2.jsx
+++ frontend/src/pages/jiyoung2.jsx
@@ -3,6 +3,10 @@ import StoryTemplate1 from '../components/storyTemplate/storyTemplate1/storyTemp
 import StoryTemplate3 from '../components/storyTemplate/storyTemplate3/storyTemplate3';
 import StoryTemplate4 from '../components/storyTemplate/storyTemplate4/storyTemplate4';
 import StoryTemplate5 from '../components/storyTemplate/storyTemplate5/storyTemplate5';
+import WeddingTemplate1 from '../components/weddingTemplate/weddingTemplate1/weddingTemplate1';
+import WeddingTemplate2 from '../components/weddingTemplate/weddingTemplate2/weddingTemplate2';
+import WeddingTemplate3 from '../components/weddingTemplate/weddingTemplate3/weddingTemplate3';
+import WeddingTemplate4 from '../components/weddingTemplate/weddingTemplate4/weddingTemplate4';
 
 
 const Jiyoung2 = () => {
@@ -16,7 +20,12 @@ const Jiyoung2 = () => {
 
       {/* <StoryTemplate3 /> */}
       {/* <StoryTemplate4 /> */}
-      <StoryTemplate5 />
+      {/* <StoryTemplate5 /> */}
+      
+      <WeddingTemplate1 />
+      {/* <WeddingTemplate2 /> */}
+      {/* <WeddingTemplate3 /> */}
+      {/* <WeddingTemplate4 /> */}
 
     </div>
   );
