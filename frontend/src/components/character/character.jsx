e25c7dd55cc60c2e5d3e649f82b45c304affb2da
JaeHyeok <roywogur@naver.com>
2021-05-17 04:34:42 +0900

    feat/ fe / character conflictEvent / S04P31B106-220

--- frontend/src/components/character/character.jsx
+++ frontend/src/components/character/character.jsx
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from 'react';
-
+import styles from './character.module.css'
 function Character({}) {
   const [x, setX] = useState(0);
   const [left, setLeft] = useState(false);
   const [right, setRight] = useState(false);
   const [down, setDown] = useState(false);
+  const [goTemplate, setGoTemplate] = useState(false);
   let moveLeft = 0
   let moveRight = 0
   const [manMoveImages, setManMoveImages] = useState("");
@@ -35,15 +36,19 @@ function Character({}) {
   const womanDefaultImage = images[9]
   const womanRightImages = images.slice(10,14)
   const womanLeftImage = images.slice(14)
-  console.log(womanLeftImage)
+
   useEffect(()=>{
-    document.addEventListener("keydown", handleKeyPress);
-  },[moveRight,moveLeft])
+    document.addEventListener("keydown", handleKeyPress);    
+  },[])
+  useEffect(()=> {
+    checkConflict()
+  },[x])
 
   const handleKeyPress = (e) => {
     const actionX = actionXMap[e.key];
     actionX && setX(actionX);
   }
+  
   const increment = (x) => {
     setDown(false)
     setRight(true)
@@ -64,8 +69,13 @@ function Character({}) {
     moveRight = 0
     return x - 1;  
   }
+  
   const wathForward = (x) => {
     setDown(true)
+    setRight(false)
+    setLeft(false)
+    moveLeft = 0
+    moveRight = 0
     return x;
   }
   const actionXMap = {
@@ -73,77 +83,133 @@ function Character({}) {
     ArrowRight: increment,
     ArrowDown: wathForward
   }
+  const checkConflict = () => {
+    let building = document.getElementById('school')
+    let character = document.getElementById('character')
+    if (building !== null && character !== null) {
+      // let dis = Math.pow(building.x - character.x, 2) + Math.pow(building.y - character.y, 2)
+      let dis = Math.pow(building.x - character.x, 2)
+      console.log(dis)
+      if (dis>5000 && dis < 8000) {
+        setGoTemplate(true)
+      }
+      else {
+        setGoTemplate(false)
+      }
+    }
+  }
+  const onClick = () => {
+    alert("템플릿입장")
+  }
   return (
     <>
+    <div className={styles['storyBuilding']}>
+      { goTemplate === false
+      ?
+      ''
+      :
+      <div className={styles['goTemplate']}>
+        <div className={styles['goTemplate-text']}>입장하시겠습니까?</div>
+        <button className={styles['goTemplate-button']} onClick={onClick}>Click!</button>
+      </div>
+      }
+      <div className={styles['school']}>
+        <img id="school" src="/images/school-solid.svg"/>
+      </div>
+    </div>
     {(() => {
       if (left===false && right === false || down === true)
         return (
           <div onKeyPress={handleKeyPress} style ={{display: 'inline-block'}}>
-            <img src={manDefaultImage}
+            <div
               style = {{
-              width: "100px",
-              height: "100px",
-              left: x+'rem',
-              top: "500px",
-              position: 'absolute',
-            }}
-            />
-            <img src={womanDefaultImage}
-              style = {{
-              width: "100px",
-              height: "100px",
-              left: (x+2)+'rem',
-              top: "511px",
-              position: 'absolute',
-            }}
-            />
+                width: "130px",
+                height: "100px",
+                left: x+'rem',
+                top: "500px",
+                position: "absolute"
+              }}
+            >
+              <img id="character" src={manDefaultImage}
+                style = {{
+                width: "100px",
+                height: "100px",
+                left: "0px",
+                position: 'absolute',
+              }}
+              />
+              <img src={womanDefaultImage}
+                style = {{
+                width: "100px",
+                height: "100px",
+                left: "40px",
+                position: 'absolute',
+              }}
+              />
+            </div>
           </div>
         )
       else if (left === true)
         return (
           <div onKeyPress={handleKeyPress} style ={{display: 'inline-block'}}>
-            <img src={manMoveImages}
+            <div
               style = {{
-              width: "100px",
-              height: "100px",
-              left: x+'rem',
-              top: "500px",
-              position: 'absolute',
-            }}
-            />
-            <img src={womanMoveImages}
-              style = {{
-              width: "100px",
-              height: "100px",
-              left: (x+2)+'rem',
-              top: "511px",
-              position: 'absolute',
-            }}
-            />
+                width: "130px",
+                height: "100px",
+                left: x+'rem',
+                top: "500px",
+                position: "absolute"
+              }}
+            >
+              <img id="character" src={manMoveImages}
+                style = {{
+                width: "100px",
+                height: "100px",
+                left: '0px',
+                position: 'absolute',
+              }}
+              />
+              <img src={womanMoveImages}
+                style = {{
+                width: "100px",
+                height: "100px",
+                left:'40px',
+                position: 'absolute',
+              }}
+              />
+            </div>
           </div>
         )
       else if (right === true)
         return (
           <div onKeyPress={handleKeyPress} style ={{display: 'inline-block'}}>
-          <img src={manMoveImages}
-            style = {{
-            width: "100px",
-            height: "100px",
-            left: x+'rem',
-            top: "500px",
-            position: 'absolute',
-          }}
-          />
-          <img src={womanMoveImages}
-            style = {{
-            width: "100px",
-            height: "100px",
-            left: (x+2)+'rem',
-            top: "511px",
-            position: 'absolute',
-          }}
-          />
-        </div>
+            <div
+              style = {{
+                width: "130px",
+                height: "100px",
+                left: x+'rem',
+                top: "500px",
+                position: "absolute"
+              }}
+            >
+              <img id="character" src={manMoveImages}
+                style = {{
+                width: "100px",
+                height: "100px",
+                left:'0px',
+                position: 'absolute',
+              }}
+              />
+              <img src={womanMoveImages}
+                style = {{
+                width: "100px",
+                height: "100px",
+                left: '40px',
+                position: 'absolute',
+              }}
+              />
+            </div>
+          </div>
         )
     })()}
   </>
