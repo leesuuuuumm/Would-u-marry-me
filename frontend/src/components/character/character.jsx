a853f46742fef1862e6e45ece4d21cd967cf1588
JaeHyeok <roywogur@naver.com>
2021-05-18 14:52:35 +0900

    feat/ fe / character and building / S04P31B106

--- frontend/src/components/character/character.jsx
+++ frontend/src/components/character/character.jsx
@@ -6,6 +6,9 @@ function Character({}) {
   const [right, setRight] = useState(false);
   const [down, setDown] = useState(false);
   const [goTemplate, setGoTemplate] = useState(false);
+  const [goTemplate2, setGoTemplate2] = useState(false);
+  const [sticky, setSticky] = useState(false);
+  const [displayMove, setDisplayMove] = useState(false);
   let moveLeft = 0
   let moveRight = 0
   const [manMoveImages, setManMoveImages] = useState("");
@@ -57,7 +60,23 @@ function Character({}) {
     setwoManMoveImages(womanRightImages[move])
     moveRight += 1
     moveLeft = 0
-    return x + 1;
+    if (x > 58){
+      setSticky(true)
+      // setDisplayMove(true)
+      console.log(x+"sticky")
+      return x 
+    }
+    else{
+      console.log(x+"not sticky")
+      // setDown(false)
+      // setRight(true)
+      // let move = moveRight%4;
+      // setManMoveImages(manRightImages[move])
+      // setwoManMoveImages(womanRightImages[move])
+      // moveRight += 1
+      // moveLeft = 0
+      return x + 1;
+    }
   }
   const decrement = (x) => {
     setDown(false)
@@ -78,6 +97,9 @@ function Character({}) {
     moveRight = 0
     return x;
   }
+  // const displayMove = () => {
+    
+  // }
   const actionXMap = {
     ArrowLeft: decrement,
     ArrowRight: increment,
@@ -85,133 +107,169 @@ function Character({}) {
   }
   const checkConflict = () => {
     let building = document.getElementById('school')
+    let building2 = document.getElementById('hospital')
     let character = document.getElementById('character')
+
     if (building !== null && character !== null) {
-      // let dis = Math.pow(building.x - character.x, 2) + Math.pow(building.y - character.y, 2)
-      let dis = Math.pow(building.x - character.x, 2)
-      console.log(dis)
-      if (dis>5000 && dis < 8000) {
+      let dis = building.x - character.x
+      if (dis > -200 && dis < 60) {
         setGoTemplate(true)
       }
       else {
         setGoTemplate(false)
       }
     }
+    if (building2 !== null && character !== null) {
+      let dis2 = building2.x - character.x
+      if (dis2 > -200 && dis2 < 60) {
+        setGoTemplate2(true)
+      }
+      else {
+        setGoTemplate2(false)
+      }
+    }
   }
   const onClick = () => {
+    setDown(true)
     alert("템플릿입장")
   }
+  const onClick2 = () => {
+    setDown(true)
+    alert("템플릿2입장")
+  }
+
   return (
     <>
+    { sticky === false
+    ?
+    <>
     <div className={styles['storyBuilding']}>
-      { goTemplate === false
-      ?
-      ''
-      :
-      <div className={styles['goTemplate']}>
-        <div className={styles['goTemplate-text']}>입장하시겠습니까?</div>
-        <button className={styles['goTemplate-button']} onClick={onClick}>Click!</button>
-      </div>
-      }
-      <div className={styles['school']}>
-        <img id="school" src="/images/school-solid.svg"/>
-      </div>
+    { goTemplate === false
+    ?
+    ''
+    :
+    <div className={styles['goTemplate']}>
+      <div className={styles['goTemplate-text']}>입장하시겠습니까?</div>
+      <button className={styles['goTemplate-button']} onClick={onClick}>Click!</button>
     </div>
-    {(() => {
-      if (left===false && right === false || down === true)
-        return (
-          <div onKeyPress={handleKeyPress} style ={{display: 'inline-block'}}>
-            <div
+    }
+    <div className={styles['school']}>
+      <img id="school" src="/images/school-solid.svg"/>
+    </div>
+  </div>
+  <div className={styles['storyBuilding2']}>
+    { goTemplate2 === false
+    ?
+    ''
+    :
+    <div className={styles['goTemplate']}>
+      <div className={styles['goTemplate-text']}>입장하시겠습니까?</div>
+      <button className={styles['goTemplate-button']} onClick={onClick2}>Click!</button>
+    </div>
+    }
+    <div className={styles['school']}>
+      <img id="hospital" src="/images/hospital-alt-solid.svg"/>
+    </div>
+  </div>
+  {(() => {
+    if (left===false && right === false || down === true)
+      return (
+        <div onKeyPress={handleKeyPress} style ={{display: 'inline-block'}}>
+          <div
+            style = {{
+              width: "130px",
+              height: "100px",
+              left: x+'rem',
+              top: "500px",
+              position: "absolute"
+            }}
+          >
+            <img id="character" src={manDefaultImage}
               style = {{
-                width: "130px",
-                height: "100px",
-                left: x+'rem',
-                top: "500px",
-                position: "absolute"
-              }}
-            >
-              <img id="character" src={manDefaultImage}
-                style = {{
-                width: "100px",
-                height: "100px",
-                left: "0px",
-                position: 'absolute',
-              }}
-              />
-              <img src={womanDefaultImage}
-                style = {{
-                width: "100px",
-                height: "100px",
-                left: "40px",
-                position: 'absolute',
-              }}
-              />
-            </div>
-          </div>
-        )
-      else if (left === true)
-        return (
-          <div onKeyPress={handleKeyPress} style ={{display: 'inline-block'}}>
-            <div
+              width: "100px",
+              height: "100px",
+              left: "0px",
+              position: 'absolute',
+            }}
+            />
+            <img src={womanDefaultImage}
               style = {{
-                width: "130px",
-                height: "100px",
-                left: x+'rem',
-                top: "500px",
-                position: "absolute"
-              }}
-            >
-              <img id="character" src={manMoveImages}
-                style = {{
-                width: "100px",
-                height: "100px",
-                left: '0px',
-                position: 'absolute',
-              }}
-              />
-              <img src={womanMoveImages}
-                style = {{
-                width: "100px",
-                height: "100px",
-                left:'40px',
-                position: 'absolute',
-              }}
-              />
-            </div>
+              width: "100px",
+              height: "100px",
+              left: "40px",
+              position: 'absolute',
+            }}
+            />
           </div>
-        )
-      else if (right === true)
-        return (
-          <div onKeyPress={handleKeyPress} style ={{display: 'inline-block'}}>
-            <div
+        </div>
+      )
+    else if (left === true)
+      return (
+        <div onKeyPress={handleKeyPress} style ={{display: 'inline-block'}}>
+          <div
+            style = {{
+              width: "130px",
+              height: "100px",
+              left: x+'rem',
+              top: "500px",
+              position: "absolute"
+            }}
+          >
+            <img id="character" src={manMoveImages}
               style = {{
-                width: "130px",
-                height: "100px",
-                left: x+'rem',
-                top: "500px",
-                position: "absolute"
-              }}
-            >
-              <img id="character" src={manMoveImages}
-                style = {{
-                width: "100px",
-                height: "100px",
-                left:'0px',
-                position: 'absolute',
-              }}
-              />
-              <img src={womanMoveImages}
-                style = {{
-                width: "100px",
-                height: "100px",
-                left: '40px',
-                position: 'absolute',
-              }}
-              />
-            </div>
+              width: "100px",
+              height: "100px",
+              left: '0px',
+              position: 'absolute',
+            }}
+            />
+            <img src={womanMoveImages}
+              style = {{
+              width: "100px",
+              height: "100px",
+              left:'40px',
+              position: 'absolute',
+            }}
+            />
           </div>
-        )
+        </div>
+      )
+    else if (right === true) 
+
+        <div onKeyPress={handleKeyPress} style ={{display: 'inline-block'}}>
+          <div
+            style = {{
+              width: "130px",
+              height: "100px",
+              left: x+'rem',
+              top: "500px",
+              position: "absolute"
+            }}
+          >
+            <img id="character" src={manMoveImages}
+              style = {{
+              width: "100px",
+              height: "100px",
+              left:'0px',
+              position: 'absolute',
+            }}
+            />
+            <img src={womanMoveImages}
+              style = {{
+              width: "100px",
+              height: "100px",
+              left: '40px',
+              position: 'absolute',
+            }}
+            />
+          </div>
+        </div>
     })()}
+      </>
+    :
+    <div></div>
+    }
+
   </>
   )
 }
