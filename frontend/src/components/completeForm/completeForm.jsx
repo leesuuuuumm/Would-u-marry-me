26f5b8903226787472249b310a538198b1019700
AgFe <dbsdmscjf304@naver.com>
2021-05-19 16:35:50 +0900

    feat / fe / KakaoShare / S04P31B106-255

--- frontend/src/components/completeForm/completeForm.jsx
+++ frontend/src/components/completeForm/completeForm.jsx
@@ -1,55 +1,67 @@
-import React from 'react';
-import { useHistory } from 'react-router';
-import styles from './completeForm.module.css'
-
+import React from "react";
+import { useHistory } from "react-router";
+import styles from "./completeForm.module.css";
+import KakaoLinkDefault from "./KakaoLinkDefault";
 
 const CompleteForm = ({ storyBoardId }) => {
-
-  const history = useHistory()
+  const history = useHistory();
 
   const userName = localStorage.getItem("userName");
+  const key = process.env.REACT_APP_KAKAO_KEY;
+  const template = {
+    objectType: "feed",
+    content: {
+      title: userName + "의 결혼식에 초대합니다.",
+      description: userName + "의 청첩장입니다. 웹으로 이용해주세요!",
+      imageUrl: "https://d3mf3wgh9v41yo.cloudfront.net/bck_kakao-thumbnail.png",
+      link: {
+        mobileWebUrl: `https://wouldumarryme.com/${userName}/invitation/${storyBoardId}`,
+        webUrl: `https://wouldumarryme.com/${userName}/invitation/${storyBoardId}`,
+      },
+    },
+    buttons: [
+      {
+        title: "웹으로 보기",
+        link: {
+          mobileWebUrl: `https://wouldumarryme.com/${userName}/invitation/${storyBoardId}`,
+          webUrl: `https://wouldumarryme.com/${userName}/invitation/${storyBoardId}`,
+        },
+      },
+    ],
+  };
 
   const moveMyStoryBoard = () => {
     history.push(`/${userName}/storyboard`);
   };
 
   const moveCompletedStoryBoard = () => {
-    history.push(`/${userName}/invitation/${storyBoardId}`)
+    history.push(`/${userName}/invitation/${storyBoardId}`);
   };
-
-  const shareWithKakaoTalk = () => {
-    // 아래 주소로 공유하면 됨!
-    // `https://wouldumarryme.com/${userName}/invitation/${storyBoardId}`
-  };
-
-  
 
   return (
-    <div className={styles['complete-box']}>
-      <div className={styles['buttons-area']}>
+    <div className={styles["complete-box"]}>
+      <div className={styles["buttons-area"]}>
         <div>
-          <button
-            className={styles['buttons']}
-            onClick={moveMyStoryBoard}
-          >
+          <button className={styles["buttons"]} onClick={moveMyStoryBoard}>
             마이스토리보드로 이동
           </button>
         </div>
         <div>
           <button
-            className={styles['buttons']}
+            className={styles["buttons"]}
             onClick={moveCompletedStoryBoard}
           >
             완성본 보기
           </button>
         </div>
         <div>
-          <button
-            className={styles['buttons']}
-            onClick={shareWithKakaoTalk}
+          <KakaoLinkDefault
+            className="template"
+            template={template}
+            jsKey={key}
           >
-            카카오톡으로 공유하기
-          </button>
+            <button className={styles["buttons"]}>카카오톡으로 공유하기</button>
+          </KakaoLinkDefault>
         </div>
       </div>
     </div>
