cf8686ffead9c7f29576cdf32955cc39310a00fd
Jiyoung <jayyyoung321@gmail.com>
2021-05-20 16:00:05 +0900

    feat / fe / invitationWedding1 수정 / S04P31B106-266

--- frontend/src/components/invitationTemplate/wedding/invitationWedding1/invitationWedding1.jsx
+++ frontend/src/components/invitationTemplate/wedding/invitationWedding1/invitationWedding1.jsx
@@ -1,36 +1,45 @@
 import React from "react";
 import styles from "./invitationWedding1.module.css";
 
-const InvitationWedding1 = () => {
+const InvitationWedding1 = ({ setModalState, modalData }) => {
   
   return (
     <div className={styles['invitation-wedding1']}>
-      <button className={styles['cancel-button']}>
+      <button 
+        className={styles['cancel-button']}
+        onClick={() => {setModalState(false)}}
+      >
         <i className="fas fa-times"></i>
       </button>
       <div className={styles['section1']}>
         <div className={styles['image1-container']}>
-        
+          <img 
+            src={modalData.weddingCardImage.imgUrl} 
+            alt="" 
+            className={styles.image1}
+          />
         </div>
       </div>
       <div className={styles['section2']}>
         <div className={styles.date}>
-
+          <pre>{modalData.date}</pre>
         </div>
+
         <div className={styles['text1-container']}>
-
+          <pre>{modalData.firstComment}</pre>
         </div>
+        
         <div className={styles.time}>
-
+          <pre>{modalData.time}</pre>
         </div>
         <div className={styles.place}>
-
+          <pre>{modalData.place}</pre>
         </div>
         <div className={styles.map}>
-
+        {/* 지도 넣을 부분 */}
         </div>
         <div className={styles['text2-container']}>
-
+          <pre>{modalData.secondComment}</pre>
         </div>
       </div>
     </div>
