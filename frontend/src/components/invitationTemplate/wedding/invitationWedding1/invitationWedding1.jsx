5e27a9e53fd347f4f4f6fa9e17f5ae5445928fed
Jiyoung <jayyyoung321@gmail.com>
2021-05-21 01:25:35 +0900

    feat / fe / invitationWedding1 수정 / S04P31B106-266

--- frontend/src/components/invitationTemplate/wedding/invitationWedding1/invitationWedding1.jsx
+++ frontend/src/components/invitationTemplate/wedding/invitationWedding1/invitationWedding1.jsx
@@ -14,22 +14,26 @@ const InvitationWedding1 = ({ setModalState, modalData }) => {
       </button>
       <div className={styles['section1']}>
         <div className={styles['image1-container']}>
-          <img 
+          {
+            modalData.weddingCardImage.imgUrl
+            ?
+            <img 
             src={modalData.weddingCardImage.imgUrl} 
             alt="" 
             className={styles.image1}
-          />
+            />
+            :
+            <></>
+          }
         </div>
       </div>
       <div className={styles['section2']}>
         <div className={styles.date}>
           <pre>{modalData.date}</pre>
         </div>
-
         <div className={styles['text1-container']}>
           <pre>{modalData.firstComment}</pre>
         </div>
-        
         <div className={styles.time}>
           <pre>{modalData.time}</pre>
         </div>
@@ -37,10 +41,15 @@ const InvitationWedding1 = ({ setModalState, modalData }) => {
           <pre>{modalData.place}</pre>
         </div>
         <div className={styles.map}>
-        {/* 지도 넣을 부분 */}
-          <KakaoMap
-            readLocation={modalData.weddingCardMap}
-          ></KakaoMap>
+          {
+            modalData.weddingCardMap
+            ?
+            <KakaoMap
+              readLocation={modalData.weddingCardMap}
+            ></KakaoMap>
+            :
+            <></>
+          }
         </div>
         <div className={styles['text2-container']}>
           <pre>{modalData.secondComment}</pre>
